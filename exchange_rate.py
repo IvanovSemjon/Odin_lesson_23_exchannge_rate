ac22f80045d1b455f07023be4dd30e955461ff2a
IvanovSemjon <IvanovSemjon@yabdex.ru>
2025-08-29 15:04:29 +0300

    Вместо поля ввода добавлен выпадающий список

--- exchange_rate.py
+++ exchange_rate.py
@@ -1,14 +1,14 @@
 import requests
 from tkinter import *
+from tkinter import ttk
 from tkinter import messagebox as mb
 
 
 def get_exchange_rate():
-    currency = entry.get()
+    currency = combobox.get()
 
     if currency:
         try:
-            currency = currency.upper()
             response = requests.get(f"https://v6.exchangerate-api.com/v6/e48649bb9b00f650443eccb8/latest/USD")
             response.raise_for_status()
             data = response.json()
@@ -27,10 +27,12 @@ window = Tk()
 window.title("Курсы обмена валют")
 window.geometry("360x180")
 
-Label(text="Введите код валюты:").pack(padx=10, pady=10)
+Label(text="Выберите код валюты:").pack(padx=10, pady=10)
+combobox = ttk.Combobox(values=["USD", "EUR", "BYR", "AZN", "AMD", "BGN", "INR", "KZT", "SCNY", "BRL"])
+combobox.pack(padx=10, pady=10)
 
-entry = Entry()
-entry.pack(padx=10, pady=10)
+# entry = Entry()
+# entry.pack(padx=10, pady=10)
 
 Button(text="Получить курс", command=get_exchange_rate).pack(padx=10, pady=10)
 

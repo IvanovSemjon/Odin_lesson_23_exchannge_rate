0c9d346518f140e1258767edf1e1901b23cb0d32
IvanovSemjon <IvanovSemjon@yabdex.ru>
2025-08-29 15:32:03 +0300

    В метку добавлены названия валют

--- exchange_rate.py
+++ exchange_rate.py
@@ -4,6 +4,13 @@ from tkinter import ttk
 from tkinter import messagebox as mb
 
 
+def update_c_lable(event):
+    code = combobox.get()
+    name = cur[code]
+    c_label.configure(text=f"{code} - {name}")
+
+
+
 def get_exchange_rate():
     currency = combobox.get()
 
@@ -14,7 +21,8 @@ def get_exchange_rate():
             data = response.json()
             if currency in data["conversion_rates"]:
                 exchange_rate = data["conversion_rates"][currency]
-                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {currency} за один доллар.")
+                c_name = cur[currency]
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {c_name} за один доллар.")
             else:
                 mb.showerror("Ошибка", "Валюта не найдена")
         except Exception as e:
@@ -23,16 +31,32 @@ def get_exchange_rate():
         mb.showwarning("Ошибка", "Введите код валюты")
 
 
+cur = {
+    "USD": "Американский доллар", 
+    "EUR": "Евро",
+    "BYN": "Белорусский рубль", 
+    "AZN": "Азербайджанский манат",
+    "AMD": "Армянский драм",
+    "BGN": "Болгарский лев",
+    "INR": "Индийская рупия",
+    "KZT": "Казахстанский тенге",
+    "CNY": "Китайский юань",
+    "BRL": "Бразильский реал",
+    "RUB": "Российский рубль"
+    }
+
+
 window = Tk()
 window.title("Курсы обмена валют")
 window.geometry("360x180")
 
 Label(text="Выберите код валюты:").pack(padx=10, pady=10)
-combobox = ttk.Combobox(values=["USD", "EUR", "BYR", "AZN", "AMD", "BGN", "INR", "KZT", "SCNY", "BRL"])
+combobox = ttk.Combobox(values=list(cur.keys()))
 combobox.pack(padx=10, pady=10)
+combobox.bind("<<ComboboxSelected>>", update_c_lable)
 
-# entry = Entry()
-# entry.pack(padx=10, pady=10)
+c_label = ttk.Label()
+c_label.pack(padx=10, pady=10)
 
 Button(text="Получить курс", command=get_exchange_rate).pack(padx=10, pady=10)
 

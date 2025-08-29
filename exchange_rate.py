59b6f217f1771091a85f4e18365fd889cfabd117
IvanovSemjon <IvanovSemjon@yabdex.ru>
2025-08-29 14:38:36 +0300

    Создана функция get_exchange для курса валют

--- exchange_rate.py
+++ exchange_rate.py
@@ -1,24 +1,27 @@
 import requests
-import json
-import pprint
-
 from tkinter import *
 from tkinter import messagebox as mb
 
-# result = requests.get("https://v6.exchangerate-api.com/v6/e48649bb9b00f650443eccb8/latest/USD")
-# data = json.loads(result.text)
-# p = pprint.PrettyPrinter(indent=4)
-# p.pprint(data)
 
 def get_exchange_rate():
     currency = entry.get()
-    result = requests.get(f"https://v6.exchangerate-api.com/v6/e48649bb9b00f650443eccb8/latest/{currency}")
-    data = json.loads(result.text)
-    if data["result"] == "success":
-        rate = data["conversion_rates"]["RUB"]
-        mb.showinfo("Курс обмена", f"1 {currency} = {rate} RUB")
+
+    if currency:
+        try:
+            currency = currency.upper()
+            response = requests.get(f"https://v6.exchangerate-api.com/v6/e48649bb9b00f650443eccb8/latest/{currency}")
+            response.raise_for_status()
+            data = response.json()
+            if currency in data["conversion_rates"]:
+                exchange_rate = data["conversion_rates"][currency]
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate} {currency} за один доллар.")
+            else:
+                mb.showerror("Ошибка", "Валюта не найдена")
+        except Exception as e:
+            mb.showerror("Ошибка", f"Произошла ошибка --> {e}")
     else:
-        mb.showerror("Ошибка", "Валюта не найдена")
+        mb.showwarning("Ошибка", "Введите код валюты")
+
 
 window = Tk()
 window.title("Курсы обмена валют")
@@ -30,3 +33,5 @@ entry = Entry(window)
 entry.pack(padx=10, pady=10)
 
 Button(window, text="Получить курс", command=lambda: get_exchange_rate).pack(padx=10, pady=10)
+
+window.mainloop()
